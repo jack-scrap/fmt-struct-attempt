23d5d4cfd51031cc61bb72de33791dd8bf273773
jackhasakeyboard <jackhasakeyboard@gmail.com>
2024-08-12 05:56:16 -0600

    Created format struct for an item where you're attempting to do something

--- main.c
+++ main.c
@@ -1,3 +1,23 @@
+#include <stdio.h>
+
+void err(char* msg) {
+	fprintf(stderr, "Error: %s\n", msg);
+}
+
 int main(int argc, char* argv[]) {
+	if (argc == 1) {
+		err("No arguments");
+
+		return 1;
+	}
+
+	if (argc != 1 + 1) {
+		err("Wrong number of arguments");
+
+		return 1;
+	}
+
+	printf("Attempt to %s", argv[1]);
+
 	return 0;
 }
